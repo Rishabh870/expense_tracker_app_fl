e8d975e4cd9dedb13c5d4ad75bd2bc7438ee762c
Priyanshu <sahupriyanshu108@gmail.com>
2025-09-29 23:06:47 +0530

    Changed Api URL

--- lib/utils/RequestMethod.dart
+++ lib/utils/RequestMethod.dart
@@ -5,7 +5,7 @@ import 'package:expense_tracker_app_fl/providers/token_manager.dart';
 // const String baseUrl = 'http://10.0.2.2:8000';
 //  const String baseUrl = 'http://192.168.29.209:8000';
 
-  const String baseUrl = 'http://140.245.210.208'; //PRODUCTION
+  const String baseUrl = 'https://expense-api.myapico.live'; //PRODUCTION
 
 final Dio publicDio = Dio(BaseOptions(baseUrl: baseUrl));
 final Dio privateDio = Dio(BaseOptions(baseUrl: baseUrl));
